73003e9d6646b626283d47b66c46c03217c55a94
Jasmina Muharemovic <jasminamhrmvc@proton.me>
2024-11-20 16:29:38 +0100

    loading skeletons

---
+++ app/issues/loading.tsx
@@ -0,0 +1,48 @@
+import { Table, TableCell, TableColumnHeaderCell } from '@radix-ui/themes'
+import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import IssueActions from './IssueActions'
+const LoadingIssuesPage = () => {
+
+const issues = [1, 2, 3, 4]
+
+  return (
+    <div>
+    <IssueActions/>
+    <Table.Root variant="surface">
+    <Table.Header>
+      <Table.Row>
+        <TableColumnHeaderCell>Issue</TableColumnHeaderCell>
+        <TableColumnHeaderCell className="hidden md:table-cell">
+          Status
+        </TableColumnHeaderCell>
+        <TableColumnHeaderCell className="hidden md:table-cell">
+          Created
+        </TableColumnHeaderCell>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      {issues.map((issue) => (
+        <Table.Row key={issue}>
+          <TableCell>
+            <Skeleton/>
+            <div className="block md:hidden">
+            <Skeleton/>
+            </div>
+          </TableCell>
+          <TableCell className="hidden md:table-cell">
+          <Skeleton/>
+          </TableCell>
+          <TableCell className="hidden md:table-cell">
+          <Skeleton/>
+          </TableCell>
+        </Table.Row>
+      ))}
+    </Table.Body>
+  </Table.Root>
+  </div>
+  )
+}
+
+export default LoadingIssuesPage
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import { Button, Table, TableCell, TableColumnHeaderCell } from '@radix-ui/themes';
-import Link from 'next/link';
-import prisma from '@/prisma/client'
-import IssueStatusBadge from '../components/IssueStatusBadge';
+import React from "react";
+import {
+  Button,
+  Table,
+  TableCell,
+  TableColumnHeaderCell,
+} from "@radix-ui/themes";
+import Link from "next/link";
+import prisma from "@/prisma/client";
+import IssueStatusBadge from "../components/IssueStatusBadge";
+import delay from "delay";
+import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
+  
+
   const issues = await prisma.issue.findMany();
+  await delay(2000);
 
   return (
     <div>
-      <div className='mb-5'>
-        <Button>
-          <Link href="/issues/new">New Issue</Link>
-        </Button>
-      </div>
-      <Table.Root variant='surface'>
+      <IssueActions/>
+      <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
             <TableColumnHeaderCell>Issue</TableColumnHeaderCell>
-            <TableColumnHeaderCell className='hidden md:table-cell'>Status</TableColumnHeaderCell>
-            <TableColumnHeaderCell className='hidden md:table-cell'>Created</TableColumnHeaderCell>
+            <TableColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </TableColumnHeaderCell>
+            <TableColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </TableColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -27,10 +37,16 @@ const IssuesPage = async () => {
             <Table.Row key={issue.id}>
               <TableCell>
                 {issue.title}
-                <div className='block md:hidden'><IssueStatusBadge status={issue.status}/></div>
-                </TableCell>
-              <TableCell className='hidden md:table-cell'><IssueStatusBadge status={issue.status}/></TableCell>
-              <TableCell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</TableCell>
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </TableCell>
+              <TableCell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </TableCell>
             </Table.Row>
           ))}
         </Table.Body>
