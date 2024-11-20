aed010f9c956c1f67e7ac8e69637031027560277
Jasmina Muharemovic <jasminamhrmvc@proton.me>
2024-11-20 18:05:43 +0100

    add aditional loading skeletons

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -13,6 +13,7 @@ import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 
+
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
@@ -39,6 +40,8 @@ const NewIssuePage = () => {
       setError("An unexpected error occurred.");
     }
   })
+
+
 
   return (
     <div className="max-w-xl">
