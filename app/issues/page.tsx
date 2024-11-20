d0072f930142fcc1c5bd758cbaf191eb4f5a025a
Jasmina Muharemovic <jasminamhrmvc@proton.me>
2024-11-20 17:50:15 +0100

    build a styled Link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import {
-  Button,
   Table,
   TableCell,
   TableColumnHeaderCell,
 } from "@radix-ui/themes";
-import Link from "next/link";
+import Link from "../components/Link";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
