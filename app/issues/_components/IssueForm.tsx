b2bfed02ee46ef98d1cb7fe10466f818bbc5662b
Jasmina Muharemovic <jasminamhrmvc@proton.me>
2024-11-21 15:54:00 +0100

    fix caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -38,6 +38,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       setSubmitting(true);
       await axios.post("/api/issues", data);
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setSubmitting(false);
       setError("An unexpected error occurred.");
