f2cfdb0cfcd1dffc9a586f7c789eb94591f5c5d1
Jasmina Muharemovic <jasminamhrmvc@proton.me>
2024-12-03 11:29:32 +0100

    layout the dashboard

--- app/IssueChart.tsx
+++ app/IssueChart.tsx
@@ -1,7 +1,14 @@
 'use client';
-import { Card } from "@radix-ui/themes";
-import {ResponsiveContainer, BarChart, XAxis, YAxis, Bar} from 'recharts'
-import React from "react";
+
+import { Card } from '@radix-ui/themes';
+import {
+  ResponsiveContainer,
+  BarChart,
+  XAxis,
+  YAxis,
+  Bar,
+} from 'recharts';
+import React from 'react';
 
 interface Props {
   open: number;
@@ -9,23 +16,28 @@ interface Props {
   closed: number;
 }
 
-const IssueChart = ({open, inProgress, closed}: Props) => {
+const IssueChart = ({ open, inProgress, closed }: Props) => {
+  const data = [
+    { label: 'Open', value: open },
+    { label: 'In Progress', value: inProgress },
+    { label: 'Closed', value: closed },
+  ];
 
-    const data = [
-        {label: 'Open', value: open },
-        {label: 'In Progress', value: inProgress},
-        {label: 'Closed', value: closed}
-    ]
-
-  return <Card>
-    <ResponsiveContainer width="100%" height={300}>
-    <BarChart data={data}>
-        <XAxis dataKey="label"/>
-        <YAxis/>
-        <Bar dataKey="value" barSize={60} style={{fill: 'var(--accent-9)'}}></Bar>
-    </BarChart>
-    </ResponsiveContainer>
-  </Card>
+  return (
+    <Card>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data}>
+          <XAxis dataKey="label" />
+          <YAxis />
+          <Bar
+            dataKey="value"
+            barSize={60}
+            style={{ fill: 'var(--accent-9)' }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </Card>
+  );
 };
 
 export default IssueChart;
